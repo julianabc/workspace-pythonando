fc9335de290747af6242bb177fca9a6457e20bc6
julianabc <julianabc@id.uff.br>
2022-01-11 12:35:39 -0300

    Consertando a chave

--- imobi/settings.py
+++ imobi/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)# tem que ser booleano
 
 ALLOWED_HOSTS = []
 
