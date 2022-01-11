fb3a1d96414a72bf0773a6490592a22ef267ac91
julianabc <julianabc@id.uff.br>
2022-01-11 11:16:10 -0300

    Conserto da chave

--- imobi/settings.py
+++ imobi/settings.py
@@ -2,6 +2,10 @@ from pathlib import Path
 import os
 from django.contrib.messages import constants # responsável pela emissão de mensagens como alerta para o usuario
 
+# para a chave
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3@^smqp36ahs)vt%*y0a^r)ku9qu18))g(p(c$+6wc7e1h1#2e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
